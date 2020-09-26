48aeb44c115cd120a3f16743350f0078ab610756
unknown <hvscdev@programmer.net>
2020-09-26 17:51:17 +0100

    v1.3.0
    Template literals! What a wonder.

--- src/main/java/io/github/cybertelx/hello/HelloMod.java
+++ src/main/java/io/github/cybertelx/hello/HelloMod.java
@@ -1,26 +1,47 @@
 package io.github.cybertelx.hello;
 
-import java.util.ArrayList;
-
-import java.util.List;
-
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.options.KeyBinding;
 
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.JanksonConfigSerializer;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.Toml4jConfigSerializer;
 
+import java.time.format.DateTimeFormatter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.ZonedDateTime;    
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.HashMap; // import the HashMap class
+
+import org.apache.commons.lang3.text.StrSubstitutor;
+
 public class HelloMod implements ClientModInitializer {
+	
+	static Properties prop;
 	
 	@Override
 	public void onInitializeClient() {
 		// on initialize client
+		/*
+		Properties prop = new Properties();
+		InputStream input = null;
+		
+		try {
+
+		    input = new FileInputStream("./gradle.properties");
+
+		    // load a properties file
+		    prop.load(input);
+		} catch (IOException ex) {
+		    ex.printStackTrace();
+		}*/
 
 		System.out.println("Hello! initialized - Developed by cybertelx");
 		AutoConfig.register(ModConfig.class, Toml4jConfigSerializer::new);
@@ -37,26 +58,67 @@ public class HelloMod implements ClientModInitializer {
     public static void log(Level level, String message) {
         LOGGER.log(level, "[" + MOD_NAME + "] " + message);
     }
+    
+    @SuppressWarnings("unchecked")
+	private static String resolveString(MinecraftClient client, String message) {
+    	DateTimeFormatter hour12 = DateTimeFormatter.ofPattern("hh:mm a");  
+		DateTimeFormatter hour24 = DateTimeFormatter.ofPattern("HH:mm");  
+		ZonedDateTime now = ZonedDateTime.now();  
+		   
+		Map valuesMap = new HashMap<String, String>();
+		// Username
+		
+		String username = client.player.getGameProfile().getName();
+		valuesMap.put("username", username);
+		valuesMap.put("name", username);
+		
+		// Servers
+		// Removed due to weird bug thingymajig.
+		/*
+		String ip = client.world.getServer().getServerIp();
+		String port = Integer.toString(client.world.getServer().getServerPort());
+		
+		valuesMap.put("ip", ip);
+		valuesMap.put("port", port);
+		valuesMap.put("server", ip.concat(":").concat(port));*/
+		
+		// Time
+		valuesMap.put("time", hour12.format(now));
+		valuesMap.put("24time", hour24.format(now));
+		
+		// Minecraft info
+		valuesMap.put("mc_version", client.getGame().getVersion().getName());
+		
+		// Misc
+		valuesMap.put("os", System.getProperty("os.name"));
+		valuesMap.put("java_version", System.getProperty("java.version"));
 
-    public static void gameJoined(MinecraftClient client){
+		StrSubstitutor sub = new StrSubstitutor(valuesMap);
+		String resolved = sub.replace(message);
+		
+		return resolved;
+    }
+
+	public static void gameJoined(MinecraftClient client){
         isLoading = false;
         isInGame = true;
         
         ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
         
         if (config.toggle == true) {
-        	if (client.isInSingleplayer()) {
+        	if (client.isInSingleplayer() == true) {
+        		String resolved = resolveString(client, config.message);
         		if (config.sendSP == true) {
-        			client.player.sendChatMessage(config.message);
+        			client.player.sendChatMessage(resolved);
         		}
         	} else {
-        		client.player.sendChatMessage(config.message);
+        		String resolved = resolveString(client, config.message);
+        		log(Level.DEBUG, config.message);
+        		log(Level.DEBUG, resolved);
+        		client.player.sendChatMessage(resolved);
         	}
         	
         }
-        /*for (int i = 0; i < 5; i++){
-            client.player.sendMessage(Text.of("Test spam message"), false);
-        }*/
     }
 
     public static void gameLeft(){
